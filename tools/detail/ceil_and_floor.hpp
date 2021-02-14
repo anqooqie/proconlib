03a9d2e410592a4a54b7214a6b0f6d2081996dd0
anqooqie <zzzfuku1991@gmail.com>
2021-02-15 00:21:24 +0900

    Make tools::floor and tools::ceil return the common type of lhs and rhs

--- tools/detail/ceil_and_floor.hpp
+++ tools/detail/ceil_and_floor.hpp
@@ -1,54 +1,58 @@
 #ifndef TOOLS_DETAIL_CEIL_AND_FLOOR_HPP
 #define TOOLS_DETAIL_CEIL_AND_FLOOR_HPP
 
+#include <type_traits>
+
 namespace tools {
 
   /**
    * floor function
    * License: CC0
    * @author anqooqie
-   * @param <T> type of the operands
+   * @param <M> type of `lhs`
+   * @param <N> type of `rhs`
    * @param lhs left hand side operand
    * @param rhs right hand side operand
    * @return $\left\lfloor \frac{\mathrm{lhs}}{\mathrm{rhs}} \right\rfloor$
    */
-  template <typename T>
-  constexpr T floor(const T& lhs, const T& rhs);
+  template <typename M, typename N>
+  constexpr ::std::common_type_t<M, N> floor(const M& lhs, const N& rhs);
 
   /**
    * ceiling function
    * License: CC0
    * @author anqooqie
-   * @param <T> type of the operands
+   * @param <M> type of `lhs`
+   * @param <N> type of `rhs`
    * @param lhs left hand side operand
    * @param rhs right hand side operand
    * @return $\left\lceil \frac{\mathrm{lhs}}{\mathrm{rhs}} \right\rceil$
    */
-  template <typename T>
-  constexpr T ceil(const T& lhs, const T& rhs);
+  template <typename M, typename N>
+  constexpr ::std::common_type_t<M, N> ceil(const M& lhs, const N& rhs);
   
-  template <typename T>
-  constexpr T floor(const T& lhs, const T& rhs) {
+  template <typename M, typename N>
+  constexpr ::std::common_type_t<M, N> floor(const M& lhs, const N& rhs) {
     return
       lhs >= 0 && rhs >= 0 ?
         lhs / rhs :
       lhs < 0 && rhs >= 0 ?
-        -::tools::ceil<T>(-lhs, rhs) :
+        -::tools::ceil(-lhs, rhs) :
       lhs >= 0 && rhs < 0 ?
-        -::tools::ceil<T>(lhs, -rhs) :
-        ::tools::floor<T>(-lhs, -rhs);
+        -::tools::ceil(lhs, -rhs) :
+        ::tools::floor(-lhs, -rhs);
   }
   
-  template <typename T>
-  constexpr T ceil(const T& lhs, const T& rhs) {
+  template <typename M, typename N>
+  constexpr ::std::common_type_t<M, N> ceil(const M& lhs, const N& rhs) {
     return
       lhs >= 0 && rhs >= 0 ?
         (lhs - 1 + rhs) / rhs :
       lhs < 0 && rhs >= 0 ?
-        -::tools::floor<T>(-lhs, rhs) :
+        -::tools::floor(-lhs, rhs) :
       lhs >= 0 && rhs < 0 ?
-        -::tools::floor<T>(lhs, -rhs) :
-        ::tools::ceil<T>(-lhs, -rhs);
+        -::tools::floor(lhs, -rhs) :
+        ::tools::ceil(-lhs, -rhs);
   }
 }
 
