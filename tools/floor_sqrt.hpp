c56e4ee830253bb959b4c2453fd2a4405892a3d8
anqooqie <zzzfuku1991@gmail.com>
2021-02-15 00:21:36 +0900

    Add tools::floor_sqrt

---
+++ tools/floor_sqrt.hpp
@@ -0,0 +1,36 @@
+#ifndef TOOLS_FLOOR_SQRT_HPP
+#define TOOLS_FLOOR_SQRT_HPP
+
+#include <cassert>
+
+namespace tools {
+
+  /**
+   * computes $\left\lfloor \sqrt{x} \right\rfloor$
+   * License: CC0
+   * @author anqooqie
+   * @param x input
+   * @return $\left\lfloor \sqrt{x} \right\rfloor$
+   */
+  template <typename T>
+  T floor_sqrt(T n) {
+    assert(n >= 0);
+
+    T ok = 0;
+    T ng;
+    for (ng = 1; ng * ng <= n; ng *= 2);
+
+    while (ng - ok > 1) {
+      const T mid = (ok + ng) / 2;
+      if (mid * mid <= n) {
+        ok = mid;
+      } else {
+        ng = mid;
+      }
+    }
+
+    return ok;
+  }
+}
+
+#endif
