e52e993acc0bdddfede2cc704e54e9c8945eb6a9
anqooqie <zzzfuku1991@gmail.com>
2021-02-28 14:24:15 +0900

    Add tests of `tools::ceil_log` and `tools::floor_log`

---
+++ tests/ceil_log.test.cpp
@@ -0,0 +1,53 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A"
+
+#include <cstdlib>
+#include <iostream>
+#include "tools/ceil_log.hpp"
+
+void assert_that(const bool cond) {
+  if (!cond) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+int main() {
+  assert_that(tools::ceil_log(2, 1) == 0);
+  assert_that(tools::ceil_log(2, 2) == 1);
+  assert_that(tools::ceil_log(2, 3) == 2);
+  assert_that(tools::ceil_log(2, 4) == 2);
+  assert_that(tools::ceil_log(2, 5) == 3);
+  assert_that(tools::ceil_log(2, 6) == 3);
+  assert_that(tools::ceil_log(2, 7) == 3);
+  assert_that(tools::ceil_log(2, 8) == 3);
+  assert_that(tools::ceil_log(2, 9) == 4);
+  assert_that(tools::ceil_log(3, 1) == 0);
+  assert_that(tools::ceil_log(3, 2) == 1);
+  assert_that(tools::ceil_log(3, 3) == 1);
+  assert_that(tools::ceil_log(3, 4) == 2);
+  assert_that(tools::ceil_log(3, 5) == 2);
+  assert_that(tools::ceil_log(3, 6) == 2);
+  assert_that(tools::ceil_log(3, 7) == 2);
+  assert_that(tools::ceil_log(3, 8) == 2);
+  assert_that(tools::ceil_log(3, 9) == 2);
+  assert_that(tools::ceil_log(4, 1) == 0);
+  assert_that(tools::ceil_log(4, 2) == 1);
+  assert_that(tools::ceil_log(4, 3) == 1);
+  assert_that(tools::ceil_log(4, 4) == 1);
+  assert_that(tools::ceil_log(4, 5) == 2);
+  assert_that(tools::ceil_log(4, 6) == 2);
+  assert_that(tools::ceil_log(4, 7) == 2);
+  assert_that(tools::ceil_log(4, 8) == 2);
+  assert_that(tools::ceil_log(4, 9) == 2);
+  assert_that(tools::ceil_log(2, 576460752303423487) == 59);
+  assert_that(tools::ceil_log(2, 576460752303423488) == 59);
+  assert_that(tools::ceil_log(2, 576460752303423489) == 60);
+  assert_that(tools::ceil_log(2, 1000000000000000000) == 60);
+  assert_that(tools::ceil_log(1000000000000000000, 1) == 0);
+  assert_that(tools::ceil_log(1000000000000000000, 2) == 1);
+  assert_that(tools::ceil_log(999999999999999999, 999999999999999998) == 1);
+  assert_that(tools::ceil_log(999999999999999999, 999999999999999999) == 1);
+  assert_that(tools::ceil_log(999999999999999999, 1000000000000000000) == 2);
+
+  std::cout << "Hello World" << '\n';
+  return 0;
+}
---
+++ tests/floor_log.test.cpp
@@ -0,0 +1,53 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A"
+
+#include <cstdlib>
+#include <iostream>
+#include "tools/floor_log.hpp"
+
+void assert_that(const bool cond) {
+  if (!cond) {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+int main() {
+  assert_that(tools::floor_log(2, 1) == 0);
+  assert_that(tools::floor_log(2, 2) == 1);
+  assert_that(tools::floor_log(2, 3) == 1);
+  assert_that(tools::floor_log(2, 4) == 2);
+  assert_that(tools::floor_log(2, 5) == 2);
+  assert_that(tools::floor_log(2, 6) == 2);
+  assert_that(tools::floor_log(2, 7) == 2);
+  assert_that(tools::floor_log(2, 8) == 3);
+  assert_that(tools::floor_log(2, 9) == 3);
+  assert_that(tools::floor_log(3, 1) == 0);
+  assert_that(tools::floor_log(3, 2) == 0);
+  assert_that(tools::floor_log(3, 3) == 1);
+  assert_that(tools::floor_log(3, 4) == 1);
+  assert_that(tools::floor_log(3, 5) == 1);
+  assert_that(tools::floor_log(3, 6) == 1);
+  assert_that(tools::floor_log(3, 7) == 1);
+  assert_that(tools::floor_log(3, 8) == 1);
+  assert_that(tools::floor_log(3, 9) == 2);
+  assert_that(tools::floor_log(4, 1) == 0);
+  assert_that(tools::floor_log(4, 2) == 0);
+  assert_that(tools::floor_log(4, 3) == 0);
+  assert_that(tools::floor_log(4, 4) == 1);
+  assert_that(tools::floor_log(4, 5) == 1);
+  assert_that(tools::floor_log(4, 6) == 1);
+  assert_that(tools::floor_log(4, 7) == 1);
+  assert_that(tools::floor_log(4, 8) == 1);
+  assert_that(tools::floor_log(4, 9) == 1);
+  assert_that(tools::floor_log(2, 576460752303423487) == 58);
+  assert_that(tools::floor_log(2, 576460752303423488) == 59);
+  assert_that(tools::floor_log(2, 576460752303423489) == 59);
+  assert_that(tools::floor_log(2, 1000000000000000000) == 59);
+  assert_that(tools::floor_log(1000000000000000000, 1) == 0);
+  assert_that(tools::floor_log(1000000000000000000, 2) == 0);
+  assert_that(tools::floor_log(999999999999999999, 999999999999999998) == 0);
+  assert_that(tools::floor_log(999999999999999999, 999999999999999999) == 1);
+  assert_that(tools::floor_log(999999999999999999, 1000000000000000000) == 1);
+
+  std::cout << "Hello World" << '\n';
+  return 0;
+}
