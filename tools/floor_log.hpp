d7a1c9b2e567475e8260ea20604d2a1dd9c3029e
anqooqie <zzzfuku1991@gmail.com>
2021-03-07 02:52:41 +0900

    As for `tools::floor_log` and `tools::ceil_log`, clarify the upper bound of inputs

--- tools/floor_log.hpp
+++ tools/floor_log.hpp
@@ -19,8 +19,8 @@ namespace tools {
    */
   template <typename M, typename N>
   ::std::common_type_t<M, N> floor_log(const M& base, const N& antilogarithm) {
-    assert(base > 1);
-    assert(antilogarithm > 0);
+    assert(2 <= base && base <= 1000000000000000000);
+    assert(1 <= antilogarithm && antilogarithm <= 1000000000000000000);
 
     const ::std::common_type_t<M, N> threshold = ::tools::floor(antilogarithm, base);
     ::std::common_type_t<M, N> logarithm = 0;
