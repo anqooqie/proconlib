a33b11dba52e8cebd616f5c71745077695db3782
anqooqie <zzzfuku1991@gmail.com>
2021-11-27 17:53:56 +0900

    Add a test for compile options of QCFium's method

---
+++ tests/qcfium.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A"
+
+#include <iostream>
+#include "tools/qcfium.hpp"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  std::cout << "Hello World" << '\n';
+  return 0;
+}
