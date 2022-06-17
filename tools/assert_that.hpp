5559a75f0b0e7b7984ff7bec1ec2905bd75b169d
anqooqie <zzzfuku1991@gmail.com>
2022-06-17 23:50:47 +0900

    Add required #include on `tools::assert_that`

--- tools/assert_that.hpp
+++ tools/assert_that.hpp
@@ -1,10 +1,13 @@
 #ifndef TOOLS_ASSERT_THAT_HPP
 #define TOOLS_ASSERT_THAT_HPP
 
+#include <iostream>
+#include <cstdlib>
+
 #define assert_that(cond) do {\
   if (!(cond)) {\
-    std::cerr << __FILE__ << ':' << __LINE__ << ": " << __func__ << ": Assertion `" << #cond << "' failed." << '\n';\
-    std::exit(EXIT_FAILURE);\
+    ::std::cerr << __FILE__ << ':' << __LINE__ << ": " << __func__ << ": Assertion `" << #cond << "' failed." << '\n';\
+    ::std::exit(EXIT_FAILURE);\
   }\
 } while (false)
 
