3fa425e6cc36d420ab0576e9691234448a1da85f
anqooqie <zzzfuku1991@gmail.com>
2022-11-12 08:27:21 +0900

    Fix comment

--- template/input_generator.cpp
+++ template/input_generator.cpp
@@ -10,12 +10,12 @@ int main() {
   // [Guide]
   // 
   // How to get a random integer in [1, 10000]:
-  // std::uniform_int_distribution<i64> dist(1, 10000);
+  // std::uniform_int_distribution<ll> dist(1, 10000);
   // std::cout << dist(engine) << '\n';
   // 
   // How to sample random 3 integers from a vector:
-  // std::vector<i64> population({1, 2, 3, 4, 5});
-  // std::vector<i64> sample;
+  // std::vector<ll> population({1, 2, 3, 4, 5});
+  // std::vector<ll> sample;
   // std::sample(population.begin(), population.end(), std::back_inserter(sample), 3, engine);
 
   return 0;
