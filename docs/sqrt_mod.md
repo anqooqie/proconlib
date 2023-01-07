f378408f700d19f5905bc0bf577fb1b129ba0877
anqooqie <zzzfuku1991@gmail.com>
2023-01-07 17:06:29 +0900

    Add `tools::sqrt_mod`

---
+++ docs/sqrt_mod.md
@@ -0,0 +1,23 @@
+---
+title: $\sqrt{x} \pmod{P}$
+documentation_of: //tools/sqrt_mod.hpp
+---
+
+```cpp
+std::optional<long long> sqrt_mod(long long x, long long P);
+```
+
+It returns one of $y$s such that $y^2 \equiv x \pmod{P}$ if it exists.
+If such $y$ does not exist, it returns `std::nullopt`.
+
+## Constraints
+- $P$ is a prime.
+
+## Time Complexity
+- $O\left(\left(\log P\right)^2\right)$
+
+## License
+- CC0
+
+## Author
+- anqooqie
---
+++ tests/sqrt_mod.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sqrt_mod"
+
+#include <iostream>
+#include "tools/sqrt_mod.hpp"
+
+using ll = long long;
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  ll T;
+  std::cin >> T;
+  for (ll t = 0; t < T; ++t) {
+    ll Y, P;
+    std::cin >> Y >> P;
+    const auto answer = ::tools::sqrt_mod(Y, P);
+    std::cout << (answer ? *answer : -1) << '\n';
+  }
+
+  return 0;
+}
