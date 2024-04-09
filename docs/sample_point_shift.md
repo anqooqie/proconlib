aaceff53cd1863b25f12c98ece92a4263bfa02d3
anqooqie <zzzfuku1991@gmail.com>
2024-04-09 22:29:44 +0900

    Add O(N) variants of `tools::sample_point_shift`

--- docs/sample_point_shift.md
+++ docs/sample_point_shift.md
@@ -3,35 +3,89 @@ title: Shift of sampling points of polynomial
 documentation_of: //tools/sample_point_shift.hpp
 ---
 
+## (1)
 ```cpp
-(1)
 template <typename InputIterator>
-std::vector<typename std::iterator_traits<InputIterator>::value_type> sample_point_shift(InputIterator begin, InputIterator end, typename std::iterator_traits<InputIterator>::value_type c, int M);
-
-(2)
-template <typename T>
-std::vector<T> sample_point_shift(std::initializer_list<T> il, T c, int M);
+typename std::iterator_traits<InputIterator>::value_type sample_point_shift(InputIterator begin, InputIterator end, typename std::iterator_traits<InputIterator>::value_type c);
 ```
 
-- (1)
-    - Given sampling points $f(0), f(1), \ldots, f(N - 1) \pmod{P}$ of polynomial $f(x)$ of degree less than $N$, it returns $f(c), f(c + 1), \ldots, f(c + M - 1) \pmod{P}$ where $P$ is `std::iterator_traits<InputIterator>::value_type::mod()`.
-- (2)
-    - It is equivalent to `sample_point_shift(il.begin(), il.end(), c, M)`.
+Given sampling points $f(0), f(1), \ldots, f(N - 1) \pmod{P}$ of polynomial $f(x)$ of degree less than $N$, it returns $f(c) \pmod{P}$ where $P$ is `std::iterator_traits<InputIterator>::value_type::mod()`.
+
+### Constraints
+- `typename std::iterator_traits<InputIterator>::value_type` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
+- $P$ is a prime.
+- $1 \leq N \leq P$
+
+### Time Complexity
+- $O(N)$
+
+### License
+- CC0
+
+### Author
+- anqooqie
+
+## (2)
+```cpp
+template <typename T>
+T sample_point_shift(std::initializer_list<T> il, T c);
+```
+
+It is equivalent to `sample_point_shift(il.begin(), il.end(), c)`.
+
+### Constraints
+- `T` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
+- $P$ is a prime.
+- $1 \leq N \leq P$
+
+### Time Complexity
+- $O(N)$
+
+### License
+- CC0
+
+### Author
+- anqooqie
+
+## (3)
+```cpp
+template <typename InputIterator>
+std::vector<typename std::iterator_traits<InputIterator>::value_type> sample_point_shift(InputIterator begin, InputIterator end, typename std::iterator_traits<InputIterator>::value_type c, int M);
+```
+
+Given sampling points $f(0), f(1), \ldots, f(N - 1) \pmod{P}$ of polynomial $f(x)$ of degree less than $N$, it returns $f(c), f(c + 1), \ldots, f(c + M - 1) \pmod{P}$ where $P$ is `std::iterator_traits<InputIterator>::value_type::mod()`.
 
 ### References
 - [Shift of Sampling Points of Polynomialの実装 \| Mathlog](https://mathlog.info/articles/3351)
 
 ### Constraints
-- (1)
-    - `typename std::iterator_traits<InputIterator>::value_type` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
-    - `begin` $\neq$ `end`
-- (2)
-    - `T` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
-    - `il` is not empty.
-- (1), (2)
-    - $P$ is a prime.
-    - $1 \leq N \leq P$
-    - $M \geq 0$
+- `typename std::iterator_traits<InputIterator>::value_type` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
+- $P$ is a prime.
+- $1 \leq N \leq P$
+- $M \geq 0$
+
+### Time Complexity
+- $O\left( (N + M) \log(N + M) \right)$
+
+### License
+- CC0
+
+### Author
+- anqooqie
+
+## (4)
+```cpp
+template <typename T>
+std::vector<T> sample_point_shift(std::initializer_list<T> il, T c, int M);
+```
+
+It is equivalent to `sample_point_shift(il.begin(), il.end(), c, M)`.
+
+### Constraints
+- `T` is `atcoder::static_modint` or `atcoder::dynamic_modint`.
+- $P$ is a prime.
+- $1 \leq N \leq P$
+- $M \geq 0$
 
 ### Time Complexity
 - $O\left( (N + M) \log(N + M) \right)$
--- tools/sample_point_shift.hpp
+++ tools/sample_point_shift.hpp
@@ -3,12 +3,15 @@
 
 #include <type_traits>
 #include <iterator>
-#include <vector>
 #include <cassert>
 #include <array>
+#include <vector>
 #include <initializer_list>
+#include <algorithm>
 #include "tools/is_prime.hpp"
 #include "tools/fact_mod_cache.hpp"
+#include "tools/online_cumsum.hpp"
+#include "tools/monoid.hpp"
 #include "tools/convolution.hpp"
 
 namespace tools {
@@ -19,78 +22,128 @@ namespace tools {
       ::std::random_access_iterator_tag,
       typename ::std::iterator_traits<RandomAccessIterator>::iterator_category
     >,
-    ::std::vector<typename ::std::iterator_traits<RandomAccessIterator>::value_type>
-  > sample_point_shift(const RandomAccessIterator begin, const RandomAccessIterator end, const typename ::std::iterator_traits<RandomAccessIterator>::value_type c, const int M) {
-    assert(begin < end);
+    typename ::std::iterator_traits<RandomAccessIterator>::value_type
+  > sample_point_shift(const RandomAccessIterator begin, const RandomAccessIterator end, const typename ::std::iterator_traits<RandomAccessIterator>::value_type c) {
     using T = typename ::std::iterator_traits<RandomAccessIterator>::value_type;
     assert(::tools::is_prime(T::mod()));
-    const int N = ::std::distance(begin, end) - 1;
-    assert(N < T::mod());
+    const int N = ::std::distance(begin, end);
+    assert(1 <= N && N <= T::mod());
+    ::tools::fact_mod_cache<T> cache;
+    const ::std::array<T, 2> minus_1_pow = {T(1), T(-1)};
+
+    ::tools::online_cumsum<::tools::monoid::multiplies<T>, true> nl(N);
+    ::tools::online_cumsum<::tools::monoid::multiplies<T>, false> nr(N);
+    {
+      T last = c;
+      for (int i = 0; i < N; ++i, --last) {
+        nl[i] = nr[i] = last;
+      }
+    }
+
+    T answer(0);
+    for (int i = 0; i < N; ++i) {
+      answer += nl.prod(0, i) * nr.prod(i + 1, N) * minus_1_pow[(N - 1 - i) & 1] * cache.fact_inv(N - 1 - i) * cache.fact_inv(i) * begin[i];
+    }
+
+    return answer;
+  }
+
+  template <typename InputIterator>
+  ::std::enable_if_t<
+    !::std::is_base_of_v<
+      ::std::random_access_iterator_tag,
+      typename ::std::iterator_traits<InputIterator>::iterator_category
+    >,
+    typename ::std::iterator_traits<InputIterator>::value_type
+  > sample_point_shift(const InputIterator begin, const InputIterator end, const typename ::std::iterator_traits<InputIterator>::value_type c) {
+    using T = typename ::std::iterator_traits<InputIterator>::value_type;
+    const ::std::vector<T> samples(begin, end);
+    return ::tools::sample_point_shift(samples.begin(), samples.end(), c);
+  }
+
+  template <typename T>
+  T sample_point_shift(const ::std::initializer_list<T> il, const T c) {
+    return ::tools::sample_point_shift(il.begin(), il.end(), c);
+  }
+
+  template <typename RandomAccessIterator>
+  ::std::enable_if_t<
+    ::std::is_base_of_v<
+      ::std::random_access_iterator_tag,
+      typename ::std::iterator_traits<RandomAccessIterator>::iterator_category
+    >,
+    ::std::vector<typename ::std::iterator_traits<RandomAccessIterator>::value_type>
+  > sample_point_shift(const RandomAccessIterator begin, const RandomAccessIterator end, const typename ::std::iterator_traits<RandomAccessIterator>::value_type c, const int M) {
+    using T = typename ::std::iterator_traits<RandomAccessIterator>::value_type;
+    assert(::tools::is_prime(T::mod()));
+    const int N = ::std::distance(begin, end);
+    assert(1 <= N && N <= T::mod());
     assert(0 <= M);
     if (M == 0) return ::std::vector<T>{};
-    const int m = ::std::min(M, T::mod()) - 1;
+    if (M == 1) return ::std::vector<T>{::tools::sample_point_shift(begin, end, c)};
     ::tools::fact_mod_cache<T> cache;
     const ::std::array<T, 2> minus_1_pow = {T(1), T(-1)};
 
     ::std::vector<T> c1;
     {
-      ::std::vector<T> a1(N + 1);
-      for (int i = 0; i <= N; ++i) {
+      ::std::vector<T> a1(N);
+      for (int i = 0; i < N; ++i) {
         a1[i] = begin[i] * cache.fact_inv(i);
       }
 
-      ::std::vector<T> b1(N + 1);
-      for (int i = 0; i <= N; ++i) {
+      ::std::vector<T> b1(N);
+      for (int i = 0; i < N; ++i) {
         b1[i] = minus_1_pow[i & 1] * cache.fact_inv(i);
       }
 
       ::tools::convolution(a1.begin(), a1.end(), b1.begin(), b1.end(), ::std::back_inserter(c1));
-      c1.resize(N + 1);
+      c1.resize(N);
     }
 
     ::std::vector<T> c2;
     {
-      ::std::vector<T> a2(N + 1);
-      for (int i = 0; i <= N; ++i) {
-        a2[i] = c1[N - i] * cache.fact(N - i);
+      ::std::vector<T> a2(N);
+      for (int i = 0; i < N; ++i) {
+        a2[i] = c1[N - 1 - i] * cache.fact(N - 1 - i);
       }
 
-      ::std::vector<T> b2(N + 1);
+      ::std::vector<T> b2(N);
       b2[0] = T(1);
       T b = c;
-      for (int i = 1; i <= N; ++i, --b) {
+      for (int i = 1; i < N; ++i, --b) {
         b2[i] = b2[i - 1] * b;
       }
-      for (int i = 0; i <= N; ++i) {
+      for (int i = 0; i < N; ++i) {
         b2[i] *= cache.fact_inv(i);
       }
 
       ::tools::convolution(a2.begin(), a2.end(), b2.begin(), b2.end(), ::std::back_inserter(c2));
-      c2.resize(N + 1);
+      c2.resize(N);
       ::std::reverse(c2.begin(), c2.end());
-      for (int i = 0; i <= N; ++i) {
+      for (int i = 0; i < N; ++i) {
         c2[i] *= cache.fact_inv(i);
       }
     }
 
     ::std::vector<T> c3;
+    const int m = ::std::min(M, T::mod());
     {
       const auto& a3 = c2;
 
-      ::std::vector<T> b3(m + 1);
-      for (int i = 0; i <= m; ++i) {
+      ::std::vector<T> b3(m);
+      for (int i = 0; i < m; ++i) {
         b3[i] = cache.fact_inv(i);
       }
 
       ::tools::convolution(a3.begin(), a3.end(), b3.begin(), b3.end(), ::std::back_inserter(c3));
-      c3.resize(m + 1);
-      for (int i = 0; i <= m; ++i) {
+      c3.resize(m);
+      for (int i = 0; i < m; ++i) {
         c3[i] *= cache.fact(i);
       }
     }
 
-    for (int i = ::tools::ssize(c3); i < M; ++i) {
-      c3[i] = c3[T(i).val()];
+    for (int i = m; i < M; ++i) {
+      c3[i] = c3[i % T::mod()];
     }
 
     return c3;
