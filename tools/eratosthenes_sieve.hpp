6bfc378c0f8c233187767904b80b8770c88faeac
anqooqie <zzzfuku1991@gmail.com>
2024-09-28 22:59:33 +0900

    Speed up `tools::eratosthenes_sieve`

--- tools/eratosthenes_sieve.hpp
+++ tools/eratosthenes_sieve.hpp
@@ -1,18 +1,36 @@
 #ifndef TOOLS_ERATOSTHENES_SIEVE_HPP
 #define TOOLS_ERATOSTHENES_SIEVE_HPP
 
+#include <array>
+#include <cstdint>
 #include <vector>
 #include <cstddef>
+#include <iterator>
 #include <cassert>
+#include <algorithm>
 
 namespace tools {
   template <typename T>
   class eratosthenes_sieve {
-    ::std::vector<bool> m_is_prime;
-
-    int n() const {
-      return this->m_is_prime.size() - 1;
-    }
+    constexpr static ::std::array<::std::uint64_t, 15> init = {
+      UINT64_C(0b0010100000100010100010100010000010100000100010100010100010000010),
+      UINT64_C(0b1000001010000010001010001010001000001010000010001010001010001000),
+      UINT64_C(0b1000100000101000001000101000101000100000101000001000101000101000),
+      UINT64_C(0b0010100010000010100000100010100010100010000010100000100010100010),
+      UINT64_C(0b1010001010001000001010000010001010001010001000001010000010001010),
+      UINT64_C(0b1000101000101000100000101000001000101000101000100000101000001000),
+      UINT64_C(0b0000100010100010100010000010100000100010100010100010000010100000),
+      UINT64_C(0b1010000010001010001010001000001010000010001010001010001000001010),
+      UINT64_C(0b0000101000001000101000101000100000101000001000101000101000100000),
+      UINT64_C(0b0010000010100000100010100010100010000010100000100010100010100010),
+      UINT64_C(0b1010001000001010000010001010001010001000001010000010001010001010),
+      UINT64_C(0b1000101000100000101000001000101000101000100000101000001000101000),
+      UINT64_C(0b0010100010100010000010100000100010100010100010000010100000100010),
+      UINT64_C(0b0010001010001010001000001010000010001010001010001000001010000010),
+      UINT64_C(0b1000001000101000101000100000101000001000101000101000100000101000),
+    };
+    ::std::vector<::std::uint64_t> m_is_prime;
+    int m_n;
 
   public:
     class prime_iterable {
@@ -36,14 +54,14 @@ namespace tools {
 
         iterator() = default;
         iterator(::tools::eratosthenes_sieve<T> const * const parent, const int p) : m_parent(parent), m_p(p) {
-          for (; this->m_p <= parent->n() && !parent->m_is_prime[this->m_p]; ++this->m_p);
+          for (; this->m_p <= parent->m_n && !parent->is_prime(this->m_p); ++this->m_p);
         }
 
         value_type operator*() const {
           return this->m_p;
         }
         iterator& operator++() {
-          for (++this->m_p; this->m_p <= this->m_parent->n() && !this->m_parent->m_is_prime[this->m_p]; ++this->m_p);
+          for (++this->m_p; this->m_p <= this->m_parent->m_n && !this->m_parent->is_prime(this->m_p); ++this->m_p);
           return *this;
         }
         iterator operator++(int) {
@@ -73,270 +91,259 @@ namespace tools {
     };
 
     eratosthenes_sieve() = default;
-    explicit eratosthenes_sieve(const int n) : m_is_prime(n + 1, true) {
+    explicit eratosthenes_sieve(const int n) : m_n(n) {
       assert(n >= 1);
-
-      this->m_is_prime[0] = false;
-      this->m_is_prime[1] = false;
-      for (int i = 4; i <= n; i += 2) this->m_is_prime[i] = false;
-      for (int i = 9; i <= n; i += 6) this->m_is_prime[i] = false;
-      {
-        int i = 25;
-        while (true) {
-          if (n < i) break;
-          this->m_is_prime[i] = false;
-          i += 10;
-          if (n < i) break;
-          this->m_is_prime[i] = false;
-          i += 20;
-        }
+      this->m_is_prime.reserve((n >> 6) + 1);
+      for (int i = 0; i <= n; i += 960) {
+        ::std::copy(init.begin(), n < i + 959 ? ::std::next(init.begin(), (n >> 6) % 15 + 1) : init.end(), ::std::back_inserter(this->m_is_prime));
       }
+      this->m_is_prime[0] ^= UINT64_C(0b101110);
 
       int i = 7;
       while (true) {
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 7
+        if (this->is_prime(i)) { // 7
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 7 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 7 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 7 * 1
             j += i * 6;
             if (n < j) break;
           }
         }
         i += 4;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 11
+        if (this->is_prime(i)) { // 11
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 11 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 11 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 11 * 7
             j += i * 4;
             if (n < j) break;
           }
         }
         i += 2;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 13
+        if (this->is_prime(i)) { // 13
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 13 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 13 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 13 * 11
             j += i + i;
             if (n < j) break;
           }
         }
         i += 4;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 17
+        if (this->is_prime(i)) { // 17
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 17 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 17 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 17 * 13
             j += i * 4;
             if (n < j) break;
           }
         }
         i += 2;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 19
+        if (this->is_prime(i)) { // 19
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 19 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 19 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 19 * 17
             j += i + i;
             if (n < j) break;
           }
         }
         i += 4;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 23
+        if (this->is_prime(i)) { // 23
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 23 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 23 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 23 * 19
             j += i * 4;
             if (n < j) break;
           }
         }
         i += 6;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 29
+        if (this->is_prime(i)) { // 29
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 29 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 29
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 29 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 29 * 23
             j += i * 6;
             if (n < j) break;
           }
         }
         i += 2;
         if (n < i * i) break;
-        if (this->m_is_prime[i]) { // 1
+        if (this->is_prime(i)) { // 1
           int j = i * i;
           while (true) {
-            this->m_is_prime[j] = false; // 1 * 1
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 1
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 7
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 7
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 11
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 11
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 13
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 13
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 17
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 17
             j += i + i;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 19
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 19
             j += i * 4;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 23
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 23
             j += i * 6;
             if (n < j) break;
-            this->m_is_prime[j] = false; // 1 * 29
+            this->m_is_prime[j >> 6] &= ~(UINT64_C(1) << (j & 0b111111)); // 1 * 29
             j += i + i;
             if (n < j) break;
           }
@@ -345,13 +352,13 @@ namespace tools {
       }
     }
 
-    bool is_prime(const int i) const {
-      assert(1 <= i && i <= this->n());
-      return this->m_is_prime[i];
+    inline bool is_prime(const int i) const {
+      assert(1 <= i && i <= this->m_n);
+      return (this->m_is_prime[i >> 6] >> (i & 0b111111)) & 1;
     }
 
     prime_iterable prime_range(const int l, const int r) const {
-      assert(1 <= l && l <= r && r <= this->n());
+      assert(1 <= l && l <= r && r <= this->m_n);
       return prime_iterable(this, l, r);
     }
   };
