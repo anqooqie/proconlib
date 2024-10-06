61aefea28f3b97ec5740c6ba08e50b77683bb741
anqooqie <zzzfuku1991@gmail.com>
2024-10-07 00:03:37 +0900

    Add `tools::superset_zeta`, `tools::superset_moebius`, `tools::subset_zeta` and `tools::subset_moebius`

---
+++ tests/superset_zeta.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A"
+
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include "tools/assert_that.hpp"
+#include "tools/superset_zeta.hpp"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  for (int N = 0; N <= 20; ++N) {
+    std::vector<int> a(N);
+    for (int i = 0; i < N; ++i) {
+      a[i] = 1 << i;
+    }
+
+    std::vector<int> b(N, 0);
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
+        if ((i & j) == i) b[i] += a[j];
+      }
+    }
+
+    std::vector<int> actual_b;
+    tools::superset_zeta(a.begin(), a.end(), std::back_inserter(actual_b));
+    assert_that(actual_b == b);
+
+    tools::superset_zeta(a.begin(), a.end());
+    assert_that(a == b);
+  }
+
+  std::cout << "Hello World" << '\n';
+  return 0;
+}
---
+++ tools/subset_zeta.hpp
@@ -0,0 +1,35 @@
+#ifndef TOOLS_SUBSET_ZETA_HPP
+#define TOOLS_SUBSET_ZETA_HPP
+
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include "tools/pow2.hpp"
+
+namespace tools {
+  template <typename RandomAccessIterator>
+  void subset_zeta(const RandomAccessIterator begin, const RandomAccessIterator end) {
+    const int N = end - begin;
+
+    for (int w = 0; ::tools::pow2(w) < N; ++w) {
+      for (int i = 0; ; i += ::tools::pow2(w)) {
+        for (; !((i >> w) & 1); ++i) {
+          if (!(i + ::tools::pow2(w) < N)) goto NEXT_W;
+          begin[i + ::tools::pow2(w)] += begin[i];
+        }
+      }
+    NEXT_W:
+      ;
+    }
+  }
+
+  template <typename InputIterator, typename OutputIterator>
+  void subset_zeta(const InputIterator begin, const InputIterator end, const OutputIterator result) {
+    using T = typename ::std::iterator_traits<InputIterator>::value_type;
+    ::std::vector<T> a(begin, end);
+    ::tools::subset_zeta(a.begin(), a.end());
+    ::std::move(a.begin(), a.end(), result);
+  }
+}
+
+#endif
